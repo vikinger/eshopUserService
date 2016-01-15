5807301b334679eb873457556c9ea66941c8526e
Viktor <viktor-p89@web.de>
2016-01-15 17:12:30 +0100

    Annotationen hinzugefügt damit der Service vom Zuul Server gefunden wird

--- eshopUserService/src/main/java/de/eshop/userservice/EshopUserServiceApplication.java
+++ eshopUserService/src/main/java/de/eshop/userservice/EshopUserServiceApplication.java
@@ -2,10 +2,16 @@ package de.eshop.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@RibbonClient("eshopUserService")
 public class EshopUserServiceApplication {
 
     public static void main(String[] args) {
